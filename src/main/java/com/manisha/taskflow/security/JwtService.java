265af7524512004f209ae5eb0c7a2b1fad113e4f
Manisha Gangadevi <manishagangadevi1@gmail.com>
2026-05-26 09:21:48 +0530

    feat: add Dockerfile, README and fix JWT imports

--- src/main/java/com/manisha/taskflow/security/JwtService.java
+++ src/main/java/com/manisha/taskflow/security/JwtService.java
@@ -1,8 +1,10 @@
 package com.manisha.taskflow.security;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.keys.HmacJHA;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
